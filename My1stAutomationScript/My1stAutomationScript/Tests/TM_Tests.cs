e5b27cb3c561c331432f2a517f550937914970ee
JL_Yeoh <37150682+WOD56Tech0@users.noreply.github.com>
2021-10-14 23:20:00 +0800

    fix: changes for the objects and the login page function

--- My1stAutomationScript/My1stAutomationScript/Tests/TM_Tests.cs
+++ My1stAutomationScript/My1stAutomationScript/Tests/TM_Tests.cs
@@ -12,40 +12,29 @@ namespace NUnit_Test_Script
     [Parallelizable]
     class TM_Tests : CommonDriver
     {
+        // Home Page object initialization and definition
+        HomePage homePageObj = new HomePage();
+        // TM Page object initialization and definition
+        TMPage tmPageObj = new TMPage();
 
         [Test, Order(1), Description("Check if the user is able to create Time record with valid data")]
         public void CreateTMTest()
         {
-            // Home Page object initialization and definition
-            HomePage homePageObj = new HomePage();
             homePageObj.GoToTMPage(testDriver);
-
-            // TM Page object initialization and definition
-            TMPage tmPageObj = new TMPage();
             tmPageObj.CreateTM(testDriver);
         }
 
         [Test, Order(2), Description("Check if the user is able to edit Time record with valid data")]
         public void EditTMTest()
         {
-            // Home Page object initialization and definition
-            HomePage homePageObj = new HomePage();
             homePageObj.GoToTMPage(testDriver);
-
-            // Edit Time
-            TMPage tmPageObj = new TMPage();
             tmPageObj.EditTM(testDriver);
         }
 
         [Test, Order(3), Description("Check if the user is able to delete Material record")]
         public void DeleteTMTest()
         {
-            // Home Page object initialization and definition
-            HomePage homePageObj = new HomePage();
             homePageObj.GoToTMPage(testDriver);
-
-            // Delete Material
-            TMPage tmPageObj = new TMPage();
             tmPageObj.DeleteTM(testDriver);
         }
 
